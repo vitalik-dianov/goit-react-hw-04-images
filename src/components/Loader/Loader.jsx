a3a63561720c903413b69135858551b1cfdc7d64
Vitaliy Dianov <vitalik.dianv@gmail.com>
2022-09-07 13:48:18 +0300

    update loader color

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom';
 import { ThreeDots } from 'react-loader-spinner';
+import { theme } from 'theme';
 import { Overlay } from './Loader.styled';
 export const Loader = () => {
   return ReactDOM.createPortal(
     <Overlay>
-      <ThreeDots />
+      <ThreeDots color={theme.colors.primary}/>
     </Overlay>,
     document.querySelector('#loader-root')
   );
